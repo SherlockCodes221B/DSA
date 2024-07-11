a9af644aaafc0874defcad964acc2d33242852b2
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-11 17:05:40 +0530

    11/7/2024

---
+++ Greedy/knapsack.py
@@ -0,0 +1,27 @@
+'''
+Time complexity NlogN
+
+'''
+
+
+class Knapsack():
+    def __init__(self,weight, profit) -> None:
+        self.weight = weight
+        self.profit = profit
+        self.cost = profit / weight
+def knapsck(sack_weight,arr):
+    arr.sort(key=lambda x:(x.profit/x.weight), reverse = True)
+    final = 0
+    for current_sack in arr:
+        if current_sack.weight <= sack_weight:
+            sack_weight -= current_sack.weight
+            final += current_sack.profit
+        else:
+            final += current_sack.profit * sack_weight/current_sack.weight
+    return final
+
+if __name__ == "__main__":
+    sack_weight = 50
+    arr = [Knapsack(60,10), Knapsack(100,20), Knapsack(120,30) ]
+    so_answer_is = knapsck(sack_weight,arr)
+    print(f"Anwser is {so_answer_is}")
