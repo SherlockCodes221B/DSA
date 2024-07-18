620165898635f812ffe1ef1899d51a52832bd204
SherlockCodes221B <shreeshastudy@gmail.com>
2024-07-18 10:04:51 +0530

    18/7/2024

--- DataStructures/Tree-DataStructure/dfs.py
+++ DataStructures/Tree-DataStructure/dfs.py
@@ -1,1 +1,26 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right= None
+from collections import deque
+def BFS(root):
+    if root == None:
+        return 
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        print(node.value, end=' ')
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+# Create a binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
 
+# Call DFS
+BFS(root)  # Output: 1 2 4 5 3
